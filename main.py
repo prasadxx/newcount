4b354a4be958e8c8bbda29221e04cabb761b1c24
vivekaprasad <96980843+vivekaprasad@users.noreply.github.com>
2022-09-08 18:31:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def set_timer(bot, message):
     global stoptimer
     try:
             dt1 = datetime.now()
-            dt2 = datetime(2022,2,7,00,00,00)
+            dt2 = datetime(2022,12,5,00,00,00)
             dt3 = int((dt2 - dt1).total_seconds())
             user_input_time = dt3
             user_input_event = str("🔥🔥උසස් පෙළ විභාගයට තව🔥🔥")
